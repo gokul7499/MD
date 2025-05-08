5acb2cbd1f48f322f9341e5d02d4dfdd76f5c6bc
sama gudaghe <samagudaghe44@gmail.com>
2025-05-08 11:46:23 +0530

    chages

--- src/App.js
+++ src/App.js
@@ -60,6 +60,8 @@ function App() {
 
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+
             <Route path="/shop" element={<Shop />} />
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/login" element={<LoginSignUpForm setUserDetails={setUserDetails} />} />
